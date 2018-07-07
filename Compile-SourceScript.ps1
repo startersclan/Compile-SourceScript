3035cc81b23b179150b40b874a8ca1fc23e1f9e4
Joel Timothy Oh <joeltimothyoh@live.com>
2018-07-08 02:39:24 +0800

    Rename main function. Clean up code.

--- Compile-SourceScript.ps1
+++ Compile-SourceScript.ps1
@@ -3,7 +3,7 @@
     $File
 )
 
-function Compile {
+function Compile-SourceScript {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$False)]
@@ -11,7 +11,7 @@ function Compile {
         $File
     )
 
-    # Get script and compile.exe items. Define variables
+    # Process script
     $script = Get-Item -Path $File
     $script_exts = '.sp', '.sma'
     if ($script_exts -notcontains $script.Extension) {
@@ -20,18 +20,21 @@ function Compile {
 
     try {
         Write-Host "Running compile wrapper" -ForegroundColor Cyan
+
+        # Define variables
         $scripting_dir = $script.DirectoryName
-        #Write-Host "Directory: $scripting_dir" -ForegroundColor Yellow
         $compile_bin_name = 'compile.exe'
         $compile_core_items_names = 'compile.exe','spcomp.exe','compile.dat','amxxpc','amxxpc.exe','amxxpc_osx','amxxpc32.dll','compile.sh'
         $compile_bin = Get-Item -Path (Join-Path $scripting_dir $compile_bin_name)
-        Write-Host "Compiler: $($compile_bin.FullName)" -ForegroundColor Yellow
         $compiled_dir_name = 'compiled'
         $compiled_dir = Join-Path $scripting_dir $compiled_dir_name
         $temp_scripting_dir_name = 'scripting.tempmove'
         $temp_scripting_dir = Join-Path $scripting_dir $temp_scripting_dir_name
         $plugins_dir_name = 'plugins'
         $plugins_dir = Join-Path (Split-Path $scripting_dir -Parent) $plugins_dir_name
+
+        #Write-Host "Directory: $scripting_dir" -ForegroundColor Yellow
+        Write-Host "Compiler: $($compile_bin.FullName)" -ForegroundColor Yellow
 
         # Get and move all items except script and compile core items to temp scripting folder
         $items_excluded = Get-ChildItem -Path $scripting_dir -File | ? { $compile_core_items_names -notcontains $_.Name -And $script.Name -ne $_.Name }
@@ -54,14 +57,13 @@ function Compile {
             Remove-Item -Path $temp_scripting_dir
         }
 
-        #Write-Host "------------------" -ForegroundColor Yellow
         # Get all items in compiled folder after compilation by hash
         $compiled_items_post = Get-ChildItem $compiled_dir -Recurse -Force | select *, @{name='md5'; expression={(Get-FileHash $_.fullname -Algorithm MD5).hash}}
 
         # Get items with differing hashes
         $hashes_diff_obj = Compare-object -ReferenceObject $compiled_items_pre.md5 -DifferenceObject $compiled_items_post.md5 #| % { $compiled_items_post | ? { $_.md }
         $compiled_items_diff = $compiled_items_post | ? { $hashes_diff_obj.inputobject -contains $_.md5 }
-        #Write-Host "------------------" -ForegroundColor Green
+
         if ($compiled_items_diff) {
             Write-Host "New/Updated plugins:" -ForegroundColor Green
             #$compiled_items_diff | select *
@@ -69,7 +71,6 @@ function Compile {
             # Copy items to plugins folder
             New-Item -Path $plugins_dir -ItemType Directory -Force | Out-Null
             $compiled_items_diff | % { Copy-Item -Path $_.FullName -Destination $plugins_dir -Recurse -Confirm }
-            #Get-Item -Path Join-Path $compiled_items_diff.DirectoryName
         }else {
             Write-Host "No new/updated plugins found. No items were copied." -ForegroundColor Magenta
         }
@@ -81,5 +82,4 @@ function Compile {
 
 }
 
-
-Compile -File $File
+Compile-SourceScript -File $File
