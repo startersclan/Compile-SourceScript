5b21a0089c601912f950eb8d053a39e09c978dfa
Joel Timothy Oh <joeltimothyoh@live.com>
2019-02-20 23:00:56 +0800

    Rename variables to be camel-cased

--- Compile-SourceScript.ps1
+++ Compile-SourceScript.ps1
@@ -43,45 +43,45 @@ function Compile-SourceScript {
             Write-Host "Running compile wrapper" -ForegroundColor Cyan
 
             # Normalize paths
-            $scripting_dir = $script.DirectoryName
-            $compiled_dir = Join-Path $scripting_dir $COMPILED_DIR_NAME
-            $plugins_dir = Join-Path (Split-Path $scripting_dir -Parent) $PLUGINS_DIR_NAME
+            $scriptingDir = $script.DirectoryName
+            $compiledDir = Join-Path $scriptingDir $COMPILED_DIR_NAME
+            $pluginsDir = Join-Path (Split-Path $scriptingDir -Parent) $PLUGINS_DIR_NAME
 
             # Validate compiler binary
-            $compilerItem = Get-Item -Path (Join-Path $scripting_dir $COMPILE_BIN_NAME)
+            $compilerItem = Get-Item -Path (Join-Path $scriptingDir $COMPILE_BIN_NAME)
 
             Write-Host "Compiler: $($compilerItem.FullName)"
 
             # Get all items in compiled folder before compilation by hash
-            $compiled_dir_items_pre = Get-ChildItem $compiled_dir -Recurse -Force | ? { $_.Extension -in $PLUGIN_EXTS } | Select-Object *, @{name='md5'; expression={(Get-FileHash $_.fullname -Algorithm MD5).hash}}
+            $compiledDirItemsPre = Get-ChildItem $compiledDir -Recurse -Force | ? { $_.Extension -in $PLUGIN_EXTS } | Select-Object *, @{name='md5'; expression={(Get-FileHash $_.fullname -Algorithm MD5).hash}}
 
             # Run the compiler
             Write-Host "Compiling..." -ForegroundColor Cyan
             $epoch = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
-            $stdInFile = New-Item -Path (Join-Path $scripting_dir ".$epoch") -ItemType File -Force
+            $stdInFile = New-Item -Path (Join-Path $scriptingDir ".$epoch") -ItemType File -Force
             '1' | Out-File -FilePath $stdInFile.FullName -Force -Encoding utf8
-            Start-Process $compilerItem.FullName -ArgumentList $script.Name -WorkingDirectory $scripting_dir -RedirectStandardInput $stdInFile.FullName -Wait -NoNewWindow
+            Start-Process $compilerItem.FullName -ArgumentList $script.Name -WorkingDirectory $scriptingDir -RedirectStandardInput $stdInFile.FullName -Wait -NoNewWindow
 
             # Get all items in compiled folder after compilation by hash
-            $compiled_dir_items_post = Get-ChildItem $compiled_dir -Recurse -Force | ? { $_.Extension -in $PLUGIN_EXTS } | Select-Object *, @{name='md5'; expression={(Get-FileHash $_.FullName -Algorithm MD5).hash}}
+            $compiledDirItemsPost = Get-ChildItem $compiledDir -Recurse -Force | ? { $_.Extension -in $PLUGIN_EXTS } | Select-Object *, @{name='md5'; expression={(Get-FileHash $_.FullName -Algorithm MD5).hash}}
 
             # Get items with differing hashes
-            $hashes_diff_obj = Compare-object -ReferenceObject $compiled_dir_items_pre -DifferenceObject $compiled_dir_items_post -Property FullName, md5 | ? { $_.SideIndicator -eq '=>' }
-            $compiled_dir_items_diff = $compiled_dir_items_post | ? { $_.md5 -in $hashes_diff_obj.md5 }
+            $hashesDiffObj = Compare-object -ReferenceObject $compiledDirItemsPre -DifferenceObject $compiledDirItemsPost -Property FullName, md5 | ? { $_.SideIndicator -eq '=>' }
+            $compiledDirItemsDiff = $compiledDirItemsPost | ? { $_.md5 -in $hashesDiffObj.md5 }
 
             # Copy items to plugins folder
-            if ($compiled_dir_items_diff) {
+            if ($compiledDirItemsDiff) {
                 # List
                 Write-Host "`nCompiled plugins:" -ForegroundColor Green
-                $compiled_dir_items_diff | % { Write-Host "    $($_.Name), $($_.LastWriteTime)" -ForegroundColor Green }
+                $compiledDirItemsDiff | % { Write-Host "    $($_.Name), $($_.LastWriteTime)" -ForegroundColor Green }
 
-                New-Item -Path $plugins_dir -ItemType Directory -Force | Out-Null
-                $compiled_dir_items_diff | % {
+                New-Item -Path $pluginsDir -ItemType Directory -Force | Out-Null
+                $compiledDirItemsDiff | % {
                     if ($_.Basename -ne $script.Basename) {
                         Write-Host "`nThe scripts name does not match the compiled plugin's name." -ForegroundColor Magenta
                         return  # continue in %
                     }
-                    Copy-Item -Path $_.FullName -Destination $plugins_dir -Recurse @copyParams
+                    Copy-Item -Path $_.FullName -Destination $pluginsDir -Recurse @copyParams
                     Write-Host "Plugin copied to $($_.Fullname)" -ForegroundColor Green
                 }
             }else {
