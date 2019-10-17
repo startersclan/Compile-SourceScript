e21e1b289dd5d5ec814ea6629a789f6ba988218c
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-17 22:46:25 +0800

    Several improvements to Compile-SourceScript:
    Move processing of constants to the earlier in the function
    Revise data structure of various constants
    Standardize casing of variables depending on variable scope and type
    Add new switch -SkipWrapper for directly using compilers in the compilation process
    Improve wording and formatting of various console ouputs
    Use $PSBoundParameters where possible
    Add mandatory creation of plugin's 'compiled' directory prior to compilation
    Add throwing of error for if plugins fail to be copied to the plugins directory
    Use explicit parameters when calling certain cmdlets
    Improve formatting of code
    Improve comments on various sections of code
    Update documentation of the function

--- src/Compile-SourceScript/Public/Compile-SourceScript.ps1
+++ src/Compile-SourceScript/Public/Compile-SourceScript.ps1
@@ -4,21 +4,24 @@ function Compile-SourceScript {
     Compile-SourceScript is a wrapper for compiling SourceMod (.sp) and AMX Mod X (.sma) plugin source files for Source / GoldSource games.
 
     .DESCRIPTION
-    The script works by getting the specified plugin's source file compiled, and upon successful compilation, populating the respective mod's plugins directory with the newly compiled plugin.
+    Specified plugins source files are compiled and copied into the respective mod's plugins directory upon success.
 
     .PARAMETER File
     Path to the plugin's .sp or .sma file.
+
+    .PARAMETER SkipWrapper
+    To directly run the mod's compiler instead of using the provided wrappers (such as 'compile.exe' and 'compile.sh') in the compilation process.
 
     .PARAMETER Force
     Copies the newly compiled plugin to the plugins directory without user confirmation.
 
     .EXAMPLE
     ./Compile-SourceScript.ps1 -File ~/servers/csgo/addons/sourcemod/scripting/plugin1.sp
-    Compiles the SourceMod plugin source file 'plugin1.sp' with user confirmation for the game Counter-Strike: Global Offensive.
+    Compiles the SourceMod plugin source file 'plugin1.sp' and installs with user confirmation for the game Counter-Strike: Global Offensive.
 
     .EXAMPLE
-    ./Compile-SourceScript.ps1 -File ~/servers/cstrike/addons/amxmodx/scripting/plugin2.sma -Force
-    Compiles the AMX Mod X plugin source file 'plugin2.sma' without user confirmation for the game Counter-Strike 1.6.
+    ./Compile-SourceScript.ps1 -File ~/servers/cstrike/addons/amxmodx/scripting/plugin2.sma -SkipWrapper -Force
+    Compiles the AMX Mod X plugin source file 'plugin2.sma', skipping the compiler wrapper, and installing without user confirmation for the game Counter-Strike 1.6.
 
     .LINK
     https://github.com/theohbrothers/Compile-SourceScript
@@ -29,115 +32,180 @@ function Compile-SourceScript {
         [Parameter(Mandatory=$False)]
         [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
         $File
-    ,
+        ,
+        [Parameter(Mandatory=$False)]
+        [switch]$SkipWrapper
+        ,
         [Parameter(Mandatory=$False)]
         [switch]$Force
     )
 
     begin {
         $ErrorActionPreference = 'Stop'
-
-        # Define variables
-        $SCRIPT_EXTS = '.sp', '.sma'
-        $PLUGIN_EXTS = '.smx', '.amxx'
-        $COMPILE_WRAPPER_NAME = if ($IsWindows -Or $env:OS) { 'compile.exe' } else { 'compile.sh' }
-        $COMPILED_DIR_NAME = 'compiled'
-        $PLUGINS_DIR_NAME = 'plugins'
-
-        # Copy-Item Cmlet parameters
-        $copyParams = @{
-            Confirm = !$Force
-        }
-    }
-    process {
-        try {
-            # Process script
-            $script = Get-Item -Path $File
-            if ($script.Extension -notin $SCRIPT_EXTS) {
-                throw "File is not a .sp or .sma source file."
+        $MOD = @{
+            sourcemod = @{
+                script_ext = '.sp'
+                plugin_ext = '.smx'
+                compiled_dir_name = 'compiled'
+                plugins_dir_name = 'plugins'
+                compiler = @{
+                    windows = @{
+                        wrapper = 'compile.exe'
+                        bin = 'spcomp.exe'
+                    }
+                    others = @{
+                        wrapper = 'compile.sh'
+                        bin = 'spcomp'
+                    }
+                }
             }
+            amxmodx = @{
+                script_ext = '.sma'
+                plugin_ext = '.amxx'
+                compiled_dir_name = 'compiled'
+                plugins_dir_name = 'plugins'
+                compiler = @{
+                    windows = @{
+                        wrapper = 'compile.exe'
+                        bin = 'amxxpc.exe'
+                    }
+                    others = @{
+                        wrapper = 'compile.sh'
+                        bin = 'amxxpc'
+                    }
+                }
+            }
+        }
+        $MOD_NAME = if ([System.IO.Path]::GetExtension($PSBoundParameters['File']) -eq '.sp') { 'sourcemod' }
+                    elseif ([System.IO.Path]::GetExtension($PSBoundParameters['File']) -eq '.sma') { 'amxmodx' }
+        if (!$MOD_NAME) {
+            throw "File is not a .sp or .sma source file."
+        }
+        $COMPILER_NAME = if ($IsWindows -Or $env:OS) {
+            if ($PSBoundParameters['SkipWrapper']) { $MOD[$MOD_NAME]['compiler']['windows']['bin'] }
+            else { $MOD[$MOD_NAME]['compiler']['windows']['wrapper'] }
+        }else {
+            if ($PSBoundParameters['SkipWrapper']) { $MOD[$MOD_NAME]['compiler']['others']['bin'] }
+            else { $MOD[$MOD_NAME]['compiler']['others']['wrapper'] }
+        }
 
-            "Running compile wrapper" | Write-Host -ForegroundColor Cyan
+        try {
+            $sourceFile = Get-Item -Path $PSBoundParameters['File']
 
             # Normalize paths
-            $scriptingDir = $script.DirectoryName
-            $compiledDir = Join-Path $scriptingDir $COMPILED_DIR_NAME
-            $pluginsDir = Join-Path (Split-Path $scriptingDir -Parent) $PLUGINS_DIR_NAME
+            $SCRIPTING_DIR = $sourceFile.DirectoryName
+            $COMPILED_DIR = Join-Path $SCRIPTING_DIR $MOD[$MOD_NAME]['compiled_dir_name']
+            $PLUGINS_DIR = Join-Path (Split-Path $SCRIPTING_DIR -Parent) $MOD[$MOD_NAME]['plugins_dir_name']
 
-            # Validate compiler binary
-            $compilerItem = Get-Item -Path (Join-Path $scriptingDir $COMPILE_WRAPPER_NAME)
-            "Compiler: $($compilerItem.FullName)" | Write-Host
+             # Validate compiler binary
+            $compiler = Get-Item -Path (Join-Path $SCRIPTING_DIR $COMPILER_NAME)
+        }catch {
+            throw
+        }
+    }process {
+        try {
+            "Compiler: '$($compiler.FullName)'" | Write-Host
 
-            # Get all items in compiled folder before compilation by hash
-            $compiledDirItemsPre = Get-ChildItem $compiledDir -Recurse -Force | ? { $_.Extension -in $PLUGIN_EXTS } | Select-Object *, @{name='md5'; expression={(Get-FileHash $_.Fullname -Algorithm MD5).Hash}}
+            # Get all items in compiled directory before compilation by hash
+            $compiledDirItemsPre = Get-ChildItem -Path $COMPILED_DIR -File -Recurse -Force | ? { $_.Extension -eq $MOD[$MOD_NAME]['plugin_ext'] } | Select-Object *, @{name='md5'; expression={(Get-FileHash -Path $_.Fullname -Algorithm MD5).Hash}}
 
-            # Run the compiler
+            # Prepare for compilation
             "Compiling..." | Write-Host -ForegroundColor Cyan
             $epoch = [Math]::Floor([decimal](Get-Date(Get-Date).ToUniversalTime()-uformat "%s"))
-            $stdInFile = New-Item -Path (Join-Path $scriptingDir ".$epoch") -ItemType File -Force
+            $stdInFile = New-Item -Path (Join-Path $SCRIPTING_DIR ".$epoch") -ItemType File -Force
             '1' | Out-File -FilePath $stdInFile.FullName -Force -Encoding utf8
-            Start-Process $compilerItem.FullName -ArgumentList $script.Name -WorkingDirectory $scriptingDir -RedirectStandardInput $stdInFile.FullName -Wait -NoNewWindow
+            $processArgs = @{
+                FilePath = $compiler.FullName
+                WorkingDirectory = $SCRIPTING_DIR
+                RedirectStandardInput = $stdInFile.FullName
+                Wait = $true
+                NoNewWindow = $true
+            }
+            if ($PSBoundParameters['SkipWrapper']) {
+                $processArgs['ArgumentList'] = @(
+                    $sourceFile.Name
+                    "-o$($MOD[$MOD_NAME]['compiled_dir_name'])/$($sourceFile.Basename)$($MOD[$MOD_NAME]['plugin_ext'])"
+                )
+            }else {
+                $processArgs['ArgumentList'] = @(
+                    $sourceFile.Name
+                )
+            }
+            New-Item -Path $COMPILED_DIR -ItemType Directory -Force | Out-Null
+            # Begin compilation
+            if ($PSBoundParameters['SkipWrapper']) { "Compiling $($sourceFile.Name)..." | Write-Host -ForegroundColor Yellow }
+            Start-Process @processArgs
+            if ($PSBoundParameters['SkipWrapper']) { "End of compilation." | Write-Host -ForegroundColor Yellow }
 
-            # Get all items in compiled folder after compilation by hash
-            $compiledDirItemsPost = Get-ChildItem $compiledDir -Recurse -Force | ? { $_.Extension -in $PLUGIN_EXTS } | Select-Object *, @{name='md5'; expression={(Get-FileHash $_.FullName -Algorithm MD5).Hash}}
+            # Get all items in compiled directory after compilation by hash
+            $compiledDirItemsPost = Get-ChildItem -Path $COMPILED_DIR -File -Recurse -Force | ? { $_.Extension -eq $MOD[$MOD_NAME]['plugin_ext'] } | Select-Object *, @{name='md5'; expression={(Get-FileHash -Path $_.FullName -Algorithm MD5).Hash}}
 
             # Get items with differing hashes
             $compiledDirItemsDiff = if ($compiledDirItemsPost) {
-                if ($compiledDirItemsPre) {
-                    $hashesDiffObj = Compare-object -ReferenceObject $compiledDirItemsPre -DifferenceObject $compiledDirItemsPost -Property FullName, md5 | ? { $_.SideIndicator -eq '=>' }
-                    $compiledDirItemsPost | ? { $_.md5 -in $hashesDiffObj.md5 }
-                }else {
-                    $compiledDirItemsPost
-                }
-            }
+                                        if ($compiledDirItemsPre) {
+                                            $hashesDiffObj = Compare-object -ReferenceObject $compiledDirItemsPre -DifferenceObject $compiledDirItemsPost -Property FullName, md5 | ? { $_.SideIndicator -eq '=>' }
+                                            $compiledDirItemsPost | ? { $_.md5 -in $hashesDiffObj.md5 }
+                                        }else {
+                                            $compiledDirItemsPost
+                                        }
+                                    }
 
             if ($compiledDirItemsDiff) {
                 # List successfully compiled plugins
                 "`nNewly compiled plugins:" | Write-Host -ForegroundColor Cyan
                 $compiledDirItemsDiff | % {
-                    $compiledPluginHash = (Get-FileHash $_.FullName -Algorithm MD5).Hash
+                    $compiledPluginHash = (Get-FileHash -Path $_.FullName -Algorithm MD5).Hash
                     "    $($_.Name), $($_.LastWriteTime), $compiledPluginHash" | Write-Host -ForegroundColor White
                 }
 
-                New-Item -Path $pluginsDir -ItemType Directory -Force | Out-Null
+                New-Item -Path $PLUGINS_DIR -ItemType Directory -Force | Out-Null
+                $installationFailure = $false
                 $compiledDirItemsDiff | % {
                     "`n$($_.Name):" | Write-Host -ForegroundColor Green
-                    if ($_.Basename -ne $script.Basename) {
-                        "The plugin's name does not match the specified script's name." | Write-Host -ForegroundColor Magenta
+                    if ($_.Basename -ne $sourceFile.Basename) {
+                        "    The plugin's name does not match the specified script's name. The plugin will not copied to the plugins directory." | Write-Host -ForegroundColor Yellow
                         return  # continue in %
                     }
-                    $existingPlugin = Get-Item "$pluginsDir/$($_.Name)" -ErrorAction SilentlyContinue
+                    $existingPlugin = Get-Item -Path "$PLUGINS_DIR/$($_.Name)" -ErrorAction SilentlyContinue
                     if (!$existingPlugin) {
                         "    Plugin does not currently exist in the plugins directory." | Write-Host -ForegroundColor Yellow
                     }else {
-                        $existingPluginHash = (Get-FileHash $existingPlugin -Algorithm MD5).Hash
+                        $existingPluginHash = (Get-FileHash -Path $existingPlugin -Algorithm MD5).Hash
                         "    Existing: $($existingPlugin.LastWriteTime), $existingPluginHash" | Write-Host -ForegroundColor Yellow
                     }
                     # Display the compiled and existing plugin's file info
-                    $compiledPluginHash = (Get-FileHash $_.FullName -Algorithm MD5).Hash
+                    $compiledPluginHash = (Get-FileHash -Path $_.FullName -Algorithm MD5).Hash
                     "    Compiled: $($_.LastWriteTime), $compiledPluginHash" | Write-Host -ForegroundColor Green
 
-                    # Attempt to copy the compiled plugin to the plugins folder
-                    Copy-Item -Path $_.FullName -Destination $pluginsDir -Recurse @copyParams
+                    # Attempt to copy the compiled plugin to the plugins directory
+                    try {
+                        Copy-Item -Path $_.FullName -Destination $PLUGINS_DIR -Confirm:$(!$PSBoundParameters['Force'])
+                    }catch {
+                        "    Plugin copy error." | Write-Host -ForegroundColor Magenta
+                        $installationFailure = $true
+                        return  # continue in %
+                    }
 
-                    if ($LASTEXITCODE) { "Plugin copy error." | Write-Host -ForegroundColor Magenta; return }
                     # Alert the user on the situation of the plugin
-                    $updatedPlugin = Get-Item "$pluginsDir/$($_.Name)"
-                    $updatedPluginHash = (Get-FileHash $updatedPlugin -Algorithm MD5).Hash
-                    if ($updatedPluginHash -eq $compiledPluginHash) { "`nPlugin successfully copied to $($_.Fullname)" | Write-Host -ForegroundColor Green }
-                    else { "`nPlugin not copied." | Write-Host -ForegroundColor Magenta; return }
+                    $updatedPlugin = Get-Item -Path "$PLUGINS_DIR/$($_.Name)"
+                    $updatedPluginHash = (Get-FileHash -Path $updatedPlugin -Algorithm MD5).Hash
+                    if ($updatedPluginHash -eq $compiledPluginHash) { "`n    Plugin successfully copied to '$($_.Fullname)'" | Write-Host -ForegroundColor Green }
+                    else { "`n    Failed to copy to the plugins directory." | Write-Host -ForegroundColor Magenta; return }
+                }
+                if ($installationFailure) {
+                    throw "Failed to install one or more plugins."
                 }
             }else {
-               "`nNo newly compiled plugins found. No operations were performed." | Write-Host -ForegroundColor Magenta
+               "`nNo changes to plugins were found. No operations were performed." | Write-Host -ForegroundColor Magenta
             }
         }catch {
-            throw "Runtime error. Exception: $($_.Exception.Message)"
+            throw
         }finally {
             # Cleanup
             if ($stdInFile) {
-                Remove-Item $stdInFile -Force
+                Remove-Item -Path $stdInFile -Force
             }
-            "End of compile wrapper." | Write-Host -ForegroundColor Cyan
+            "End of Compile-SourceScript." | Write-Host -ForegroundColor Cyan
         }
     }
 }
