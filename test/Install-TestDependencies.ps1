f54eb3f2eee82f5f236c343a2580558b92523d5a
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-18 08:27:53 +0800

    Add installation of the test dependency lib32stdc++6 if necessary

--- test/Install-TestDependencies.ps1
+++ test/Install-TestDependencies.ps1
@@ -12,3 +12,20 @@ if (!$pester -or !($pester.Version -gt $pesterMinimumVersion)) {
     Install-Module -Name 'Pester' -Repository 'PSGallery' -MinimumVersion $pesterMinimumVersion -Scope CurrentUser -Force
 }
 Get-Module Pester -ListAvailable
+
+Set-StrictMode -Off
+if ($IsLinux) {
+    $provisionScript = {
+        bash -e -c "$shellScript" | Write-Host
+        if ($LASTEXITCODE) { throw "An error occurred." }
+    }
+    # Install lib32stdc++6
+    $shellScript = @"
+echo 'Installing lib32stdc++6'
+if ! dpkg -l lib32stdc++6; then
+    apt-get update && apt-get install -y lib32stdc++6
+fi
+"@
+    & $provisionScript
+}
+Set-StrictMode -Version Latest
